29680a28d33522781352a8ef4b0d6985cb4c6f18
Mahmoud <mahmoudkota97@outlook.com>
2022-12-29 13:16:50 +0200

    finish boarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'moduels/boarding_screen/boarding-screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ShopAppBoardingScreen(),
     );
   }
 }
---
+++ lib/moduels/boarding_screen/boarding-screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/moduels/login%20screen/shop_login.dart';
+import 'package:shop_app/shared/components/components.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class ShopAppBoardingScreen extends StatefulWidget {
+  const ShopAppBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ShopAppBoardingScreen> createState() => _ShopAppBoardingScreenState();
+}
+
+class _ShopAppBoardingScreenState extends State<ShopAppBoardingScreen> {
+  var boardingController = PageController();
+  List<BoardingList> modelList = [
+    BoardingList('First Screen', 'First Body', 'assets/img.png'),
+    BoardingList('Second Screen', 'Second Body', 'assets/img.png'),
+    BoardingList('Third Screen', 'Third Body', 'assets/img.png'),
+  ];
+  bool isLast = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    navigateTo(context, const ShopAppLogin());
+                  },
+                  child: const Text('Skip')),
+              Expanded(
+                child: PageView.builder(
+                  onPageChanged:(value) {
+                  if(value==modelList.length-1){
+                    isLast=true;
+                  }
+                  else{
+                    isLast=false;
+                  }
+                  },
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) =>
+                      pageViewItem(modelList[index]),
+                  controller: boardingController,
+                  itemCount: modelList.length,
+                ),
+              ),
+              Row(
+                children: [
+                  SmoothPageIndicator(
+                      effect: const ExpandingDotsEffect(
+                        activeDotColor: Colors.blue,
+                        dotWidth: 26.0,
+                        dotHeight: 15.0,
+                      ),
+                      controller: boardingController,
+                      count: modelList.length),
+                  const Spacer(),
+                  FloatingActionButton(
+                    onPressed: () {
+
+                      if(isLast){
+                        navigateAndFinish(context, ShopAppLogin());
+                      }
+                      else{
+                        boardingController.nextPage(
+                            duration: const Duration(milliseconds: 2000),
+                            curve: Curves.fastLinearToSlowEaseIn);
+                      }
+                    },
+                    child: const Icon(Icons.arrow_forward),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget pageViewItem(BoardingList model) => Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(child: Image(image: AssetImage(model.image))),
+        Text(model.title),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(model.body),
+        const SizedBox(
+          height: 50,
+        ),
+      ],
+    );
+
+class BoardingList {
+  late final String title;
+  late final String body;
+  late final String image;
+
+  BoardingList(this.title, this.body, this.image);
+}
